d8b3058b9c6b66ff37b11553e0c55eb0ef60a182
xenia <troshkinaksenia@icloud.com>
2023-01-31 19:42:21 +0300

    Key events unfinished

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 import requests
-from io import BytesIO
 
-from PIL import Image, ImageQt
+from PIL import Image
+from PIL.ImageQt import ImageQt
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
 class MapAPI(QMainWindow):
@@ -16,14 +16,13 @@ class MapAPI(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main_window.ui', self)
-        # self.setupUi(self)
-        # self.retranslateUi(self)
         self.viewComboBox.addItems(['Карта', 'Спутник', 'Гибрид'])
         self.viewComboBox.currentIndexChanged.connect(self.get_image)
 
-        self.map_ll = (37.530887, 55.703118)
-        self.map_spn = (0.01, 0.01)
-        # self.map_l = self.map_l_list[0]
+        self.map_ll = [37.530887, 55.703118]
+        self.ll_delta = 0.001
+        self.map_spn = [0.01, 0.01]
+        self.spn_delta = 0.001
         self.map_pt = []
 
         self.get_image()
@@ -39,25 +38,40 @@ class MapAPI(QMainWindow):
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
-        self.image = ImageQt.ImageQt(Image.open(BytesIO(response.content)))
-        self.map_label.setPixmap(QPixmap.fromImage(self.image))
+        map_file = "map.png"
+        try:
+            with open(map_file, "wb") as file:
+                file.write(response.content)
+        except IOError as ex:
+            print("Ошибка записи временного файла:", ex)
+            sys.exit(2)
+
+        a = ImageQt(map_file)
+        pixmap = QPixmap.fromImage(a)
+        self.map_label.setPixmap(pixmap)
+
+        # self.image = ImageQt.ImageQt(Image.open(BytesIO(response.content)))
+        # self.map_label.setPixmap(QPixmap.fromImage(self.image))
 
     # def mouseMoveEvent(self, event):
     #     self.coords.setText(f"Координаты: {event.x()}, {event.y()}")
 
     def keyPressEvent(self, event):
-        if event.key == Qt.Key_PageUp:
-            pass
-        elif event.key == Qt.Key_PageDown:
-            pass
-        elif event.key == Qt.Key_Up:
-            pass
-        elif event.key == Qt.Key_Down:
-            pass
-        elif event.key == Qt.Key_Left:
-            pass
-        elif event.key == Qt.Key_Right:
-            pass
+        if event.key() == Qt.Key_PageUp:
+            self.map_spn[0] = (self.map_spn[0] + self.spn_delta) % 90
+            self.map_spn[1] = (self.map_spn[1] + self.spn_delta) % 90
+        elif event.key() == Qt.Key_PageDown:
+            self.map_spn[0] = (self.map_spn[0] - self.spn_delta) % 90
+            self.map_spn[1] = (self.map_spn[1] - self.spn_delta) % 90
+        elif event.key() == Qt.Key_Up:
+            self.map_ll[1] = (self.map_ll[1] + self.ll_delta) % (90 if self.map_ll[1] > 0 else -90)
+        elif event.key() == Qt.Key_Down:
+            self.map_ll[1] = (self.map_ll[1] - self.ll_delta) % (90 if self.map_ll[1] > 0 else -90)
+        elif event.key() == Qt.Key_Left:
+            self.map_ll[0] = (self.map_ll[0] + self.ll_delta) % (180 if self.map_ll[1] > 0 else -180)
+        elif event.key() == Qt.Key_Right:
+            self.map_ll[0] = (self.map_ll[0] - self.ll_delta) % (180 if self.map_ll[1] > 0 else -180)
+        self.get_image()
 
 
 if __name__ == '__main__':
